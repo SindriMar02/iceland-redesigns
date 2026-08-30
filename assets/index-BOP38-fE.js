d1d77e190fc8f0702bb129ed7f1b6619508c8aa4
SindriMar02 <sindri@klubbr.is>
2026-08-30 03:37:38 +0000

    Mirror refresh: smaller hero tour card, single border, hardened autoplay

---
+++ assets/index-BOP38-fE.js
@@ -0,0 +1,1 @@
+import{g as r,aN as a}from"./index-D6qpt0oT.js";var o=a();const e=r(o);export{e as $,o as r};
---
+++ assets/use-motion-value-event-X1aJXC8u.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-D6qpt0oT.js";function e(o,t,r){n.useInsertionEffect(()=>o.on(t,r),[o,t,r])}export{e as u};
